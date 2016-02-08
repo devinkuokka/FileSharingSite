0d08d303c37e85aa15cf8092e56c0aac3794d41b
Carolyn Dean Wolf <cdeanwolf@wustl.edu>
2016-02-08 12:55:54 -0600

    no message

--- uploadScript.php
+++ uploadScript.php
@@ -3,21 +3,21 @@
 	session_start();
 	    
 	if (isset ($_POST["UploadFile"]) && isset ($_FILES["uploadedfile"]["name"])) {
-		if( !preg_match('/^[\w _\.\-]+$/', $filename) ){
+		if( !preg_match('/^[\w _\.\-]+$/', $filename) ){        //checks for valid file name
             echo "Invalid filename";
         } else {  
-            $target_dir = "/data/mod2_uploads/".$_SESSION['username']."/";
+            $target_dir = "/data/mod2_uploads/".$_SESSION['username']."/";          //sets target directory
             $target_file = $target_dir. basename($_FILES["uploadedfile"]["name"]);
             $uploadOK = 1;
 		
-            if (file_exists($target_file)) {
+            if (file_exists($target_file)) {            //checks if file already exists
                 $_SESSION['uploadMsg'] = "Sorry, file already exists.";
                 $uploadOk = 0;
             } else {
-                if (move_uploaded_file($_FILES["uploadedfile"]["tmp_name"], $target_file)) {
+                if (move_uploaded_file($_FILES["uploadedfile"]["tmp_name"], $target_file)) {    //uploads file
                     $_SESSION['uploadMsg'] = "Your file has been uploaded.";
                 } else {
-                    $_SESSION['uploadMsg'] = "Sorry, there was an error uploading your file.";
+                    $_SESSION['uploadMsg'] = "Sorry, there was an error uploading your file.";  //if there is an error uplaoding
                 }
             }
         }    
